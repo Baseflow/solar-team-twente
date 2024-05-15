b626db59af24b9555123197260356fcfe7050025
Rick van Dijk <rickavdijk@gmail.com>
2024-05-15 14:17:09 +0200

    minor fixes

--- lib/src/ui/features/more/widgets/more_options_view.dart
+++ lib/src/ui/features/more/widgets/more_options_view.dart
@@ -47,54 +47,54 @@ class MoreOptionsView extends StatelessWidget {
       ],
     );
 
-    // return BlocBuilder<ProfileCubit, ProfileState>(
-    //   builder: (BuildContext context, ProfileState state) {
-    //     return switch (state) {
-    //       ProfileLoadedState _ => Column(
-    //           children: <Widget>[
-    //             ListTile(
-    //               leading: const Icon(Icons.person),
-    //               title: Text(l10n.profileDetails),
-    //               trailing: const Icon(Icons.chevron_right),
-    //               onTap: () => context.goNamed(ProfilePage.name),
-    //             ),
-    //             ListTile(
-    //               leading: const Icon(Icons.settings),
-    //               title: Text(l10n.settings),
-    //               trailing: const Icon(Icons.chevron_right),
-    //               onTap: () => context.goNamed(SettingsPage.name),
-    //             ),
-    //             ListTile(
-    //               leading: const Icon(Icons.shield),
-    //               title: Text(l10n.termsAndConditions),
-    //               trailing: const Icon(Icons.chevron_right),
-    //               onTap: () => context.pushNamed(TermsAndConditionsPage.name),
-    //             ),
-    //             ListTile(
-    //               leading: const Icon(Icons.privacy_tip),
-    //               title: Text(l10n.privacyPolicy),
-    //               trailing: const Icon(Icons.chevron_right),
-    //               onTap: () => _launchPrivacyPolicyUrl(context),
-    //             ),
-    //             CustomAboutListTile(
-    //               child: Text(l10n.about),
-    //             ),
-    //           ],
-    //         ),
-    //       ProfileErrorState _ => Center(
-    //           child: Text(_getErrorText(state.code, l10n)),
-    //         ),
-    //       _ => const Center(child: CircularProgressIndicator()),
-    //     };
-    //   },
-    // );
+// return BlocBuilder<ProfileCubit, ProfileState>(
+//   builder: (BuildContext context, ProfileState state) {
+//     return switch (state) {
+//       ProfileLoadedState _ => Column(
+//           children: <Widget>[
+//             ListTile(
+//               leading: const Icon(Icons.person),
+//               title: Text(l10n.profileDetails),
+//               trailing: const Icon(Icons.chevron_right),
+//               onTap: () => context.goNamed(ProfilePage.name),
+//             ),
+//             ListTile(
+//               leading: const Icon(Icons.settings),
+//               title: Text(l10n.settings),
+//               trailing: const Icon(Icons.chevron_right),
+//               onTap: () => context.goNamed(SettingsPage.name),
+//             ),
+//             ListTile(
+//               leading: const Icon(Icons.shield),
+//               title: Text(l10n.termsAndConditions),
+//               trailing: const Icon(Icons.chevron_right),
+//               onTap: () => context.pushNamed(TermsAndConditionsPage.name),
+//             ),
+//             ListTile(
+//               leading: const Icon(Icons.privacy_tip),
+//               title: Text(l10n.privacyPolicy),
+//               trailing: const Icon(Icons.chevron_right),
+//               onTap: () => _launchPrivacyPolicyUrl(context),
+//             ),
+//             CustomAboutListTile(
+//               child: Text(l10n.about),
+//             ),
+//           ],
+//         ),
+//       ProfileErrorState _ => Center(
+//           child: Text(_getErrorText(state.code, l10n)),
+//         ),
+//       _ => const Center(child: CircularProgressIndicator()),
+//     };
+//   },
+// );
   }
 
-  String _getErrorText(ProfileExceptionCode code, AppLocalizations l10n) {
-    return switch (code) {
-      _ => l10n.profileNotFound,
-    };
-  }
+  // String _getErrorText(ProfileExceptionCode code, AppLocalizations l10n) {
+  //   return switch (code) {
+  //     _ => l10n.profileNotFound,
+  //   };
+  // }
 
   Future<void> _launchPrivacyPolicyUrl(BuildContext context) async {
     final Uri url = Uri.parse(Constants.appPrivacyPolicy);
