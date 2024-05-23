460fb63c542343bd08b3383c1f1bdd392b2054a4
Floyd Butselaar <floyd.butselaar@live.nl>
2024-05-23 09:11:34 +0200

    Turn DTO JSON serializable

--- lib/src/data/dto/leaderboard/leaderboard_entry_dto.dart
+++ lib/src/data/dto/leaderboard/leaderboard_entry_dto.dart
@@ -1,30 +1,23 @@
+import 'package:json_annotation/json_annotation.dart';
+
 import '../../../../core.dart';
 
 /// Data Transfer Object to represent a leaderboard entry.
+@JsonSerializable()
 class LeaderboardEntryDTO {
   /// Creates a new instance of the [LeaderboardEntryDTO].
   const LeaderboardEntryDTO({
-    required this.teamName,
-    required this.teamNumber,
+    required this.name,
+    required this.number,
     required this.position,
     required this.distance,
   });
 
-  /// Creates a new instance of the [LeaderboardEntryDTO] from a JSON object.
-  factory LeaderboardEntryDTO.fromJson(Map<String, dynamic> json) {
-    return LeaderboardEntryDTO(
-      teamName: json['name'] as String,
-      teamNumber: json['number'] as int,
-      position: json['position'] as int,
-      distance: json['distance'] as double,
-    );
-  }
-
   /// The name of the team.
-  final String teamName;
+  final String name;
 
   /// The number of the team.
-  final int teamNumber;
+  final int number;
 
   /// The position of the team on the leaderboard.
   final int position;
@@ -35,8 +28,8 @@ class LeaderboardEntryDTO {
   /// Converts the [LeaderboardEntryDTO] to a [LeaderboardEntry].
   LeaderboardEntry toEntity() {
     return LeaderboardEntry(
-      teamName: teamName,
-      teamNumber: teamNumber,
+      teamName: name,
+      teamNumber: number,
       position: position,
       distance: distance,
     );
