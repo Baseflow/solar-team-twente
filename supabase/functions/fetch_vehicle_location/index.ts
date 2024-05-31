7db217c2696a6d40d3f22698ddaa61468b08ff47
Rick van Dijk <rickavdijk@gmail.com>
2024-05-31 15:05:12 +0200

    initial implementation of ts linter

---
+++ supabase/functions/fetch_vehicle_location/index.ts
@@ -0,0 +1,41 @@
+export async function handler(req: Request) {
+  const apikey = Deno.env.get("SOLAR_API_KEY")!;
+  const baseUrl = Deno.env.get("SOLAR_BASE_URL")!;
+  const url = new URL(req.url);
+  const carId = url.searchParams.get("carId") || "DMU"
+
+  try {
+    const response = await fetch(
+      `${baseUrl}/cars/${carId}`,
+      {
+        headers: {
+          Accept: "application/json",
+          Authorization: `Bearer ${apikey}`,
+        },
+      },
+    );
+
+    const data = await response.json();
+
+    if (response.ok) {
+      return new Response(JSON.stringify(data), {
+        headers: { "content-type": "application/json" },
+        status: response.status,
+      })
+    } else {
+      return new Response(
+        response.statusText,
+        {
+          status: response.status,
+        },
+      );
+    }
+  } catch (error) {
+    console.error(error);
+    return new Response("Internal Server Error", {
+      status: 500,
+    });
+  }
+}
+
+Deno.serve(handler);
