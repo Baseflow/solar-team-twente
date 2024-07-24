f49009ed81e0688c13dc9925156d2e53e32ccb99
Rick van Dijk <rickavdijk@gmail.com>
2024-07-24 15:10:46 +0200

    get position from dto and use that in the ui

--- lib/src/core/entities/race/race_team.dart
+++ lib/src/core/entities/race/race_team.dart
@@ -3,12 +3,14 @@
 /// - the [name] of the team
 /// - the [totalDrivenKilometers] the team has driven during the race, which
 ///  is used to determine the team's position in the leaderboard.
+/// - the [position] of the team in the leaderboard.
 /// {@endtemplate}
 class RaceTeam {
   /// {@macro race_team}
   RaceTeam({
     required this.name,
     required this.totalDrivenKilometers,
+    required this.position,
   });
 
   /// The team's name.
@@ -17,4 +19,7 @@ class RaceTeam {
   /// The total distance in kilometers the team has been able to drive during
   /// the race.
   final int totalDrivenKilometers;
+
+  /// The team's position in the leaderboard.
+  final int position;
 }
--- lib/src/ui/features/leaderboard/leaderboard_preview_state.dart
+++ lib/src/ui/features/leaderboard/leaderboard_preview_state.dart
@@ -27,6 +27,18 @@ final class LeaderboardPreviewLoaded extends LeaderboardPreviewState {
   /// The stream of the leaderboard.
   final List<RaceTeam> leaderboard;
 
+  /// The Solar Team Twente in the leaderboard.
+  RaceTeam get solarTeamTwente => leaderboard.firstWhere(
+        (RaceTeam team) => team.name == Constants.solarTeamName,
+        orElse: () {
+          return RaceTeam(
+            name: 'Solar Team Twente not found',
+            totalDrivenKilometers: -1,
+            position: -1,
+          );
+        },
+      );
+
   @override
   List<Object> get props => <Object>[leaderboard];
 }
