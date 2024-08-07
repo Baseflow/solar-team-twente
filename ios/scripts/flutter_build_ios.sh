d72b776d6ca85474c82210d37c7e1cd2aadea43a
Rick van Dijk <rickavdijk@gmail.com>
2024-08-07 22:01:42 +0200

    fastlane setup for ios

---
+++ ios/scripts/flutter_build_ios.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+# Fail this script if any subcommand fails.
+set -e
+
+# Function that shows usage information
+usage()
+{
+    echo ""
+    echo "Usage: $0 -c .env.prd"
+    echo "  -c | --config-file        File containing the Flutter compile time configuration settings."
+    echo "  -h | --help               Shows this help text."
+}
+
+# Parse arguments
+while [ "$1" != "" ]; do
+    case $1 in
+        -c | --config-file)         shift
+                                    CONFIG_FILE=$1
+                                    ;;
+        -s | --secrets)             shift
+                                    SECRETS_CONFIG_FILE=$1
+                                    ;;
+        -h | --help )               usage
+                                    exit
+                                    ;;
+        * )                         usage
+                                    exit 1
+    esac
+    shift
+done
+
+# Change working directory to the project root (from the [project]/ios/fastlane directory).
+cd ../../
+
+# Make sure we work in a clean environment.
+flutter clean
+
+# Build the Flutter application.
+flutter build xcarchive \
+    --obfuscate \
+    --split-debug-info build/ios/outputs/symbols \
+    --dart-define-from-file=$CONFIG_FILE \
+    --dart-define-from-file=$SECRETS_CONFIG_FILE \
+    --no-codesign
