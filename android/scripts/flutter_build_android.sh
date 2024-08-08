2dead07d1656c7de3ceac506e667dbbd6b497154
Rick van Dijk <rickavdijk@gmail.com>
2024-08-08 12:31:18 +0200

    Android setup for fastlane including gradle updates

---
+++ android/scripts/flutter_build_android.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+# Fail this script if any subcommand fails.
+set -e
+
+# Function that shows usage information
+usage()
+{
+    echo ""
+    echo "Usage: $0 -c settings.prod"
+    echo "  -c | --config-file              File containing the Flutter compile time configuration settings."
+    echo "  -s | --secrets                  File containing the Flutter secrets configuration settings."
+    echo "  -h | --help                     Shows this help text."
+}
+
+# Parse arguments
+while [ "$1" != "" ]; do
+    case $1 in
+        -c | --config-file)             shift
+                                        CONFIG_FILE=$1
+                                        ;;
+        -s | --secrets)                 shift
+                                        SECRETS_CONFIG_FILE=$1
+                                        ;;
+        -h | --help )                   usage
+                                        exit
+                                        ;;
+        * )                             usage
+                                        exit 1
+    esac
+    shift
+done
+
+# Change working directory to the project root (from the [project]/ios/fastlane directory).
+cd ../../
+
+# Make sure we work in a clean environment.
+flutter clean
+
+# Build the Flutter application.
+flutter build appbundle \
+    --dart-define-from-file=$CONFIG_FILE \
+    --dart-define-from-file=$SECRETS_CONFIG_FILE
