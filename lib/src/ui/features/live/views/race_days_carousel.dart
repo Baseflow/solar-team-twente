225ec8d6e2f856926033d714a94c7ef41319a96b
Rick van Dijk <rickavdijk@gmail.com>
2024-08-15 14:58:12 +0200

    multiple improvements

--- lib/src/ui/features/live/views/race_days_carousel.dart
+++ lib/src/ui/features/live/views/race_days_carousel.dart
@@ -9,60 +9,60 @@ import 'count_down_view.dart';
 
 class RaceDaysCarousel extends StatelessWidget {
   const RaceDaysCarousel({super.key});
+
   static DateTime startDate = Constants.startDate;
 
   @override
   Widget build(BuildContext context) {
-    return IntrinsicHeight(
-      child: Container(
-        padding: const EdgeInsets.symmetric(vertical: Sizes.s16),
-        decoration: BoxDecoration(
-          color: context.colorScheme.surfaceContainerLow,
-          borderRadius: const BorderRadius.only(
-            topLeft: Radius.circular(20),
-            topRight: Radius.circular(20),
-          ),
+    return Container(
+      height: 200,
+      padding: const EdgeInsets.symmetric(vertical: Sizes.s16),
+      decoration: BoxDecoration(
+        color: context.colorScheme.surfaceContainerLow,
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(28),
+          topRight: Radius.circular(28),
         ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: Sizes.s16),
-              child: Row(
-                children: <Widget>[
-                  Text(
-                    DateFormat.yMMMMd(
-                      context.locale.toString(),
-                    ).format(DateTime.now()),
-                  ),
-                  const Gutter(),
-                  Text('Dag ${DateTime.now().difference(startDate).inDays}'),
-                  Expanded(
-                    child: Text(
-                      '0 km afgelegd',
-                      textAlign: TextAlign.end,
-                      style: context.textTheme.bodyMedium!.copyWith(
-                        color: context.colorScheme.primary,
-                        fontWeight: FontWeight.bold,
-                      ),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: Sizes.s16),
+            child: Row(
+              children: <Widget>[
+                Text(
+                  DateFormat.yMMMMd(
+                    context.locale.toString(),
+                  ).format(DateTime.now()),
+                ),
+                const Gutter(),
+                Text('Dag ${DateTime.now().difference(startDate).inDays}'),
+                Expanded(
+                  child: Text(
+                    '0 km afgelegd',
+                    textAlign: TextAlign.end,
+                    style: context.textTheme.bodyMedium!.copyWith(
+                      color: context.colorScheme.primary,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-            const GutterLarge(),
-            SizedBox(
-              height: 100,
-              child: PageView(
-                children: const <Widget>[
-                  CountDownView(),
-                  CountDownView(),
-                ],
-              ),
+          ),
+          const GutterLarge(),
+          SizedBox(
+            height: 100,
+            child: PageView(
+              children: const <Widget>[
+                CountDownView(),
+                CountDownView(),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
