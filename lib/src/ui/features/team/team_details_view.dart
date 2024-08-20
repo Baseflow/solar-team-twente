4366619c476791ee48bb2dffdcd54b736991f3b4
jurijjzz <jurijszuravlovs@gmail.com>
2024-08-20 13:36:38 +0200

    create states, cubit, to remove statefulWidget

--- lib/src/ui/features/team/team_details_view.dart
+++ lib/src/ui/features/team/team_details_view.dart
@@ -1,90 +1,26 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gutter/flutter_gutter.dart';
 import 'package:url_launcher/url_launcher_string.dart';
 
 import '../../constants/sizes_constants.dart';
+import '../../extensions/build_context_extensions.dart';
 import '../../localizations/generated/app_localizations.dart';
 import '../../localizations/l10n.dart';
+import 'cubit/team_details_cubit.dart';
+import 'cubit/team_details_state.dart';
 import 'team_member_view_model.dart';
 
 /// {@template team_details_view}
 /// The view displaying the details of the team competing in this edition.
 /// {@endtemplate}
-class TeamDetailsView extends StatefulWidget {
+class TeamDetailsView extends StatelessWidget {
   /// {@macro team_details_view}
-  TeamDetailsView({required BuildContext context, super.key})
-      : teamMembers = _getTeamMembers(context);
+  TeamDetailsView({required AppLocalizations l10n, super.key})
+      : teamMembers = _getTeamMembers(l10n);
   final List<TeamMember> teamMembers;
-
-  @override
-  State<TeamDetailsView> createState() => _TeamDetailsViewState();
-
-  static List<TeamMember> _getTeamMembers(BuildContext context) {
-    return <TeamMember>[
-      TeamMember(
-        imageUrl:
-            'https://a.storyblok.com/f/281680/640x450/bc27ce2695/laura-voogd_640x450.png/m/640x450/smart/filters:quality(80)',
-        name: 'Laura Voogd',
-        profession: 'Team Manager',
-        description: context.l10n.descriptionLaura,
-        linkedUrl: 'https://www.linkedin.com/in/laura-voogd-94232521a/',
-      ),
-      TeamMember(
-        imageUrl:
-            'https://a.storyblok.com/f/281680/640x450/198264f270/jeroen-pouwels_640x450.png/m/640x450/smart/filters:quality(80)',
-        name: 'Jeroen Pouwels',
-        description: context.l10n.descriptionJeroen,
-        profession: 'Account Manager',
-        linkedUrl: 'https://www.linkedin.com/in/jeroen-pouwels/',
-      ),
-      TeamMember(
-        imageUrl:
-            'https://a.storyblok.com/f/281680/640x450/dfa9cf0cd4/daan-de-jong_640x450.png/m/640x450/smart/filters:quality(80)',
-        name: 'Daan De Jong',
-        description: context.l10n.descriptionDaanDeJong,
-        profession: 'Technical / Logistics Manager',
-        linkedUrl: 'https://www.linkedin.com/in/daan-de-jong-a79b92197/',
-      ),
-      TeamMember(
-        imageUrl:
-            'https://a.storyblok.com/f/281680/640x450/02af73a59f/esmee-hulsman_640x450.png/m/640x450/smart/filters:quality(80)',
-        name: 'Esmee Hulsman',
-        description: context.l10n.descriptionEsmee,
-        profession: 'Marketing & Communications / PR',
-        linkedUrl: 'https://www.linkedin.com/in/esmee-hulsman-b6a03222b/',
-      ),
-      TeamMember(
-        imageUrl:
-            'https://a.storyblok.com/f/281680/640x450/9cc1481f31/daan-nibbering_640x450.png/m/640x450/smart/filters:quality(80)',
-        name: 'Daan Nibbering',
-        description: context.l10n.descriptionDaanNibbering,
-        profession: 'Strategist',
-        linkedUrl: 'https://www.linkedin.com/in/daan-nibbering-a09883221/',
-      ),
-      TeamMember(
-        imageUrl:
-            'https://a.storyblok.com/f/281680/640x450/7f12ad31dc/roosmarijn-meijers_640x450.png/m/640x450/smart/filters:quality(80)',
-        name: 'Roosmarijn Meijers',
-        description: context.l10n.descriptionRoosmarijn,
-        profession: 'Electrical Engineer',
-        linkedUrl: 'https://www.linkedin.com/in/roosmarijn-meijers-291415217/',
-      ),
-      TeamMember(
-        imageUrl:
-            'https://a.storyblok.com/f/281680/640x450/c05a4f11a0/vera-leeman_640x450.png/m/640x450/smart/filters:quality(80)',
-        name: 'Vera Leeman',
-        description: context.l10n.descriptionVera,
-        profession: 'Research & Development Manager',
-        linkedUrl: 'https: //www.linkedin.com/in/vera-leeman-75a688218/',
-      ),
-    ];
-  }
-}
-
-class _TeamDetailsViewState extends State<TeamDetailsView> {
-  int _currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -101,53 +37,54 @@ class _TeamDetailsViewState extends State<TeamDetailsView> {
               options: CarouselOptions(
                 aspectRatio: 1.8,
                 enlargeStrategy: CenterPageEnlargeStrategy.height,
-                onPageChanged: (int index, CarouselPageChangedReason reason) {
-                  setState(
-                    () {
-                      _currentIndex = index;
-                    },
-                  );
+                onPageChanged: (
+                  int index,
+                  CarouselPageChangedReason reason,
+                ) {
+                  context.read<TeamDetailsCubit>().selectTeamMember(index);
                 },
               ),
-              items: _imageSliders(),
+              items: _imageSliders(context),
             ),
-            Padding(
-              padding: const EdgeInsets.only(
-                top: Sizes.s32,
-                left: Sizes.s64,
-                right: Sizes.s64,
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    widget.teamMembers[_currentIndex].description,
-                    style: const TextStyle(
-                      fontSize: Sizes.s12,
-                    ),
+            BlocBuilder<TeamDetailsCubit, TeamDetailsState>(
+              builder: (BuildContext context, TeamDetailsState state) {
+                return Padding(
+                  padding: const EdgeInsets.only(
+                    top: Sizes.s32,
+                    left: Sizes.s64,
+                    right: Sizes.s64,
                   ),
-                  const Gutter(),
-                  FilledButton.tonalIcon(
-                    onPressed: () =>
-                        _launchURL(widget.teamMembers[_currentIndex].linkedUrl),
-                    style: FilledButton.styleFrom(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: Sizes.s12,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        teamMembers[state.currentIndex].description,
+                        style: context.textTheme.bodySmall,
                       ),
-                    ),
-                    label: const Text(
-                      'LinkedIn profile',
-                      style: TextStyle(
-                        fontSize: Sizes.s12,
-                        fontWeight: FontWeight.bold,
+                      const Gutter(),
+                      FilledButton.tonalIcon(
+                        onPressed: () => _launchURL(
+                          teamMembers[state.currentIndex].linkedUrl,
+                        ),
+                        style: FilledButton.styleFrom(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: Sizes.s12,
+                          ),
+                        ),
+                        label: Text(
+                          l10n.linkedIn,
+                          style: context.textTheme.bodySmall?.copyWith(
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                        icon: const Icon(Icons.open_in_new, size: Sizes.s16),
+                        iconAlignment: IconAlignment.end,
                       ),
-                    ),
-                    icon: const Icon(Icons.open_in_new, size: Sizes.s16),
-                    iconAlignment: IconAlignment.end,
+                      const Gutter(),
+                    ],
                   ),
-                  const Gutter(),
-                ],
-              ),
+                );
+              },
             ),
           ],
         ),
@@ -155,8 +92,8 @@ class _TeamDetailsViewState extends State<TeamDetailsView> {
     );
   }
 
-  List<Widget> _imageSliders() {
-    return widget.teamMembers.map(
+  List<Widget> _imageSliders(BuildContext context) {
+    return teamMembers.map(
       (TeamMember member) {
         return Column(
           crossAxisAlignment: CrossAxisAlignment.start,
@@ -206,17 +143,15 @@ class _TeamDetailsViewState extends State<TeamDetailsView> {
                           children: <Widget>[
                             Text(
                               member.name,
-                              style: const TextStyle(
+                              style: context.textTheme.bodyLarge?.copyWith(
                                 color: Colors.white,
-                                fontSize: Sizes.s16,
-                                fontWeight: FontWeight.bold,
+                                fontWeight: FontWeight.w700,
                               ),
                             ),
                             Text(
                               member.profession,
-                              style: const TextStyle(
+                              style: context.textTheme.bodySmall?.copyWith(
                                 color: Colors.white,
-                                fontSize: Sizes.s12,
                               ),
                             ),
                           ],
@@ -239,5 +174,66 @@ class _TeamDetailsViewState extends State<TeamDetailsView> {
     } else {
       throw UnimplementedError('Could not launch $url');
     }
+  }
+
+  static List<TeamMember> _getTeamMembers(AppLocalizations l10n) {
+    return <TeamMember>[
+      TeamMember(
+        imageUrl:
+            'https://a.storyblok.com/f/281680/640x450/bc27ce2695/laura-voogd_640x450.png/m/640x450/smart/filters:quality(80)',
+        name: 'Laura Voogd',
+        profession: 'Team Manager',
+        description: l10n.descriptionLaura,
+        linkedUrl: 'https://www.linkedin.com/in/laura-voogd-94232521a/',
+      ),
+      TeamMember(
+        imageUrl:
+            'https://a.storyblok.com/f/281680/640x450/198264f270/jeroen-pouwels_640x450.png/m/640x450/smart/filters:quality(80)',
+        name: 'Jeroen Pouwels',
+        description: l10n.descriptionJeroen,
+        profession: 'Account Manager',
+        linkedUrl: 'https://www.linkedin.com/in/jeroen-pouwels/',
+      ),
+      TeamMember(
+        imageUrl:
+            'https://a.storyblok.com/f/281680/640x450/dfa9cf0cd4/daan-de-jong_640x450.png/m/640x450/smart/filters:quality(80)',
+        name: 'Daan De Jong',
+        description: l10n.descriptionDaanDeJong,
+        profession: 'Technical / Logistics Manager',
+        linkedUrl: 'https://www.linkedin.com/in/daan-de-jong-a79b92197/',
+      ),
+      TeamMember(
+        imageUrl:
+            'https://a.storyblok.com/f/281680/640x450/02af73a59f/esmee-hulsman_640x450.png/m/640x450/smart/filters:quality(80)',
+        name: 'Esmee Hulsman',
+        description: l10n.descriptionEsmee,
+        profession: 'Marketing & Communications / PR',
+        linkedUrl: 'https://www.linkedin.com/in/esmee-hulsman-b6a03222b/',
+      ),
+      TeamMember(
+        imageUrl:
+            'https://a.storyblok.com/f/281680/640x450/9cc1481f31/daan-nibbering_640x450.png/m/640x450/smart/filters:quality(80)',
+        name: 'Daan Nibbering',
+        description: l10n.descriptionDaanNibbering,
+        profession: 'Strategist',
+        linkedUrl: 'https://www.linkedin.com/in/daan-nibbering-a09883221/',
+      ),
+      TeamMember(
+        imageUrl:
+            'https://a.storyblok.com/f/281680/640x450/7f12ad31dc/roosmarijn-meijers_640x450.png/m/640x450/smart/filters:quality(80)',
+        name: 'Roosmarijn Meijers',
+        description: l10n.descriptionRoosmarijn,
+        profession: 'Electrical Engineer',
+        linkedUrl: 'https://www.linkedin.com/in/roosmarijn-meijers-291415217/',
+      ),
+      TeamMember(
+        imageUrl:
+            'https://a.storyblok.com/f/281680/640x450/c05a4f11a0/vera-leeman_640x450.png/m/640x450/smart/filters:quality(80)',
+        name: 'Vera Leeman',
+        description: l10n.descriptionVera,
+        profession: 'Research & Development Manager',
+        linkedUrl: 'https://www.linkedin.com/in/vera-leeman-75a688218/',
+      ),
+    ];
   }
 }
