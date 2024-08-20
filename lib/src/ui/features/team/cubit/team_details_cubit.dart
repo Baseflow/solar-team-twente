0d100d8820236fc345b5074073dfb3adb4e2418f
jurijjzz <jurijszuravlovs@gmail.com>
2024-08-20 13:43:03 +0200

    add documentation

--- lib/src/ui/features/team/cubit/team_details_cubit.dart
+++ lib/src/ui/features/team/cubit/team_details_cubit.dart
@@ -2,9 +2,14 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'team_details_state.dart';
 
+/// {@template team_details_cubit}
+/// Manages the index of the selected team member.
+/// {@endtemplate}
 class TeamDetailsCubit extends Cubit<TeamDetailsState> {
+  /// {@macro team_details_cubit}
   TeamDetailsCubit() : super(const TeamDetailsState());
 
+  /// Selects the team member at the given index.
   void selectTeamMember(int index) {
     emit(
       TeamDetailsState(currentIndex: index),
--- lib/src/ui/features/team/cubit/team_details_state.dart
+++ lib/src/ui/features/team/cubit/team_details_state.dart
@@ -1,6 +1,10 @@
 import 'package:equatable/equatable.dart';
 
+/// {@template team_details_state}
+/// The state of the team details page.
+/// {@endtemplate}
 class TeamDetailsState extends Equatable {
+  /// {@macro team_details_state}
   const TeamDetailsState({this.currentIndex = 0});
 
   final int currentIndex;
