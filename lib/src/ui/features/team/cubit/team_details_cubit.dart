cf32e98cd38445595703c640dd53f589d431d6bb
jurijjzz <jurijszuravlovs@gmail.com>
2024-08-27 11:32:17 +0200

    fixing cubit

--- lib/src/ui/features/team/cubit/team_details_cubit.dart
+++ lib/src/ui/features/team/cubit/team_details_cubit.dart
@@ -1,18 +1,14 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'team_details_state.dart';
-
 /// {@template team_details_cubit}
 /// Manages the index of the selected team member.
 /// {@endtemplate}
-class TeamDetailsCubit extends Cubit<TeamDetailsState> {
+class TeamDetailsCubit extends Cubit<int> {
   /// {@macro team_details_cubit}
-  TeamDetailsCubit() : super(const TeamDetailsState());
+  TeamDetailsCubit() : super(0);
 
   /// Selects the team member at the given index.
   void selectTeamMember(int index) {
-    emit(
-      TeamDetailsState(currentIndex: index),
-    );
+    emit(index);
   }
 }
