f0f65bb81a2add60ec513e2b81ff8d690bdaba31
jurijjzz <jurijszuravlovs@gmail.com>
2024-09-02 10:16:19 +0200

    add admin UI

---
+++ lib/src/ui/features/admin/admin_view.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:supabase_auth_ui/supabase_auth_ui.dart';
+
+import '../../constants/sizes_constants.dart';
+import '../dashboard/views/dashboard_page.dart';
+
+class AuthView extends StatelessWidget {
+  const AuthView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Auth'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(Sizes.s12),
+        child: SupaMagicAuth(
+          onSuccess: (_) => context.go(DashboardPage.path),
+          redirectUrl: kIsWeb ? null : 'io.supabase.flutter://reset-callback/',
+        ),
+      ),
+    );
+  }
+}
