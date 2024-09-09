fb8b9ba4fd9ec241035d05d95bc62950953b14d1
Rick van Dijk <rickavdijk@gmail.com>
2024-09-09 19:19:13 +0200

    added url launcher for polarsteps

--- lib/src/ui/features/dashboard/views/dashboard_view.dart
+++ lib/src/ui/features/dashboard/views/dashboard_view.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gutter/flutter_gutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../../../constants/sizes_constants.dart';
+import '../../../localizations/l10n.dart';
 import '../../leaderboard/leaderboard_preview_container.dart';
 import '../../team/team_card.dart';
 
@@ -13,18 +15,35 @@ class DashboardView extends StatelessWidget {
   /// {@macro dashboard_view}
   const DashboardView({super.key});
 
+  static const String _urlString =
+      'https://www.polarsteps.com/SolarTeamTwente/12350380-sasol-solar-challenge';
+
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    return SafeArea(
       child: SingleChildScrollView(
         child: Padding(
-          padding: EdgeInsets.all(Sizes.s24),
+          padding: const EdgeInsets.all(Sizes.s24),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              LeaderboardPreviewContainer(),
-              GutterLarge(),
-              TeamCard(),
+              const LeaderboardPreviewContainer(),
+              const GutterLarge(),
+              const TeamCard(),
+              const GutterLarge(),
+              Center(
+                child: FilledButton.icon(
+                  onPressed: () async {
+                    final Uri uri = Uri.parse(_urlString);
+                    if (await canLaunchUrl(uri)) {
+                      await launchUrl(uri);
+                    }
+                  },
+                  icon: const Icon(Icons.open_in_new_rounded),
+                  iconAlignment: IconAlignment.end,
+                  label: Text(context.l10n.polarstepsCallToAction),
+                ),
+              ),
             ],
           ),
         ),
