bc27020a4b1aac53f5c6489cb1712507bf21a410
Rick van Dijk <rickavdijk@gmail.com>
2024-09-09 19:38:05 +0200

    minor fixes

--- lib/src/core/entities/entities.dart
+++ lib/src/core/entities/entities.dart
@@ -1,5 +1,6 @@
 export 'auth/auth.dart';
 export 'data/data_response_code.dart';
+export 'news_message.dart';
 export 'profile/profile.dart';
 export 'race/race_team.dart';
 export 'race/vehicle_location.dart';
